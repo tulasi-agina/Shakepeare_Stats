aeb263328513f4f1e289c6a05eb1b1cbc3039958
tulasi-agina <71204135+tulasi-agina@users.noreply.github.com>
2023-05-07 16:10:39 -0700

    Update ImageGrid.py

--- ImageGrid.py
+++ ImageGrid.py
@@ -6,14 +6,14 @@ st.title('Table of images')
 
 cols = st.columns(3)
 
-cols[0].image('images/row_0_col_0.png' %i, use_column_width=True)
-cols[1].image('images/row_0_col_1.png' %i, use_column_width=True)
-cols[2].image('images/row_0_col_2.png' %i, use_column_width=True)
+cols[0].image('images/row_0_col_0.png', use_column_width=True)
+cols[1].image('images/row_0_col_1.png', use_column_width=True)
+cols[2].image('images/row_0_col_2.png', use_column_width=True)
 
-cols[0].image('images/row_1_col_0.png' %i, use_column_width=True)
-cols[1].image('images/row_1_col_1.png' %i, use_column_width=True)
-cols[2].image('images/row_1_col_2.png' %i, use_column_width=True)
+cols[0].image('images/row_1_col_0.png', use_column_width=True)
+cols[1].image('images/row_1_col_1.png', use_column_width=True)
+cols[2].image('images/row_1_col_2.png', use_column_width=True)
 
-cols[0].image('images/row_2_col_0.png' %i, use_column_width=True)
-cols[1].image('images/row_2_col_1.png' %i, use_column_width=True)
-cols[2].image('images/row_2_col_2.png' %i, use_column_width=True)
+cols[0].image('images/row_2_col_0.png', use_column_width=True)
+cols[1].image('images/row_2_col_1.png', use_column_width=True)
+cols[2].image('images/row_2_col_2.png', use_column_width=True)
