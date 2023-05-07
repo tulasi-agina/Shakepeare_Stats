1b4ca05d646cfb05ab40bf77d62a9a0202705eda
tulasi-agina <71204135+tulasi-agina@users.noreply.github.com>
2023-05-07 14:50:10 -0700

    Update ImageGrid.py

--- ImageGrid.py
+++ ImageGrid.py
@@ -3,9 +3,15 @@ import streamlit as st
 
 st.title("Click on an image to navigate to the App")
 
-WS_Images = [WS_Works.jpg, WS_Sonnet.jpg, WS_Insults.jpg, WS_Sonnet.jpg]
+ 
 n_rows = 2
 n_cols = 2
 
-for image_index, ws_image in enumerate(WS_Images):
-  cols[image_index].image(ws_image)
+
+from itertools import cycle
+
+WS_Images = [WS_Works.jpg, WS_Sonnet.jpg, WS_Insults.jpg, WS_Stats.jpg] # your images here
+caption = ["Works", "Sonnets", "Insults", "Stats"] # your caption here
+cols = cycle(st.columns(3)) # st.columns here 
+for idx, ws_image in enumerate(WS_Images):
+    next(cols).image(ws_image, width=150, caption=caption[idx])
