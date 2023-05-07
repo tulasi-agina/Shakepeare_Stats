e87369807b198d1cc7f243d6ede25d39fd9a108f
tulasi-agina <71204135+tulasi-agina@users.noreply.github.com>
2023-05-07 15:36:27 -0700

    Update ImageGrid.py

--- ImageGrid.py
+++ ImageGrid.py
@@ -1,13 +1,13 @@
-import numpy as np
 import streamlit as st
-import itertools
 
-st.title("Click on an image to navigate to the App")
+st.title('Table of images')
 
-from itertools import cycle
+# to get different images in the rows and columns, have a systematic 
+# way to label your images. For mine, I have used row_{i}_col_0/1/2
 
-WS_Images = [WS_Works.jpg, WS_Sonnet.jpg, WS_Insults.jpg, WS_Stats.jpg] # your images here
-caption = ["Works", "Sonnets", "Insults", "Stats"] # your caption here
-cols = cycle(st.columns(3)) # st.columns here 
-for idx, ws_image in enumerate(WS_Images):
-    next(cols).image(ws_image, width=150, caption=caption[idx])
+for i in range(1,3): # number of rows in your table! = 2
+    cols = st.beta_columns(3) # number of columns in each row! = 3
+    # first column of the ith row
+    cols[0].image('row_%i_col_0.png' %i, use_column_width=True)
+    cols[1].image('row_%i_col_1.png' %i, use_column_width=True)
+    cols[2].image('row_%i_col_1.png' %i, use_column_width=True)
