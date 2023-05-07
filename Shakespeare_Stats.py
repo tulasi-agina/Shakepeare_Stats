b75447ccda950f540d675b4b8657f06b962410d2
tulasi-agina <71204135+tulasi-agina@users.noreply.github.com>
2023-05-07 03:38:39 -0700

    Update Shakespeare_Stats.py

--- Shakespeare_Stats.py
+++ Shakespeare_Stats.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import altair as alt
-# from wordcloud import WordCloud, STOPWORDS
+from wordcloud import WordCloud, STOPWORDS
 import glob, nltk, os, re
 from nltk.corpus import stopwords 
 import nltk
