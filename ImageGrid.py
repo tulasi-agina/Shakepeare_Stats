65b80344f222d5322204a3799c94c095b0ef05c5
tulasi-agina <71204135+tulasi-agina@users.noreply.github.com>
2023-05-07 14:38:12 -0700

    Update ImageGrid.py

--- ImageGrid.py
+++ ImageGrid.py
@@ -1,3 +1,11 @@
 import numpy as np
 import streamlit as st
 
+st.title("Click on an image to navigate to the App")
+
+WS_Images = [WS_Works.jpg, WS_Sonnet.jpg, WS_Play.jpg, WS_Sonnet.jpg]
+n_rows = 2
+n_cols = 2
+
+for image_index, ws_image in enumerate(WS_Images):
+  cols[image_index].image(ws_image)
