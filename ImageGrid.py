c91704bda5f2950b468eab8b15618ecfd904c908
tulasi-agina <71204135+tulasi-agina@users.noreply.github.com>
2023-05-07 15:56:42 -0700

    Update ImageGrid.py

--- ImageGrid.py
+++ ImageGrid.py
@@ -5,7 +5,7 @@ st.title('Table of images')
 # to get different images in the rows and columns, have a systematic 
 # way to label your images. For mine, I have used row_{i}_col_0/1/2
 
-for i in range(1,3): # number of rows in your table! = 2
+for i in range(0,3): # number of rows in your table! = 2
     cols = st.beta_columns(3) # number of columns in each row! = 3
     # first column of the ith row
     cols[0].image('images/row_%i_col_0.png' %i, use_column_width=True)
