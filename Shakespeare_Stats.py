e5a6a707a3f70e19fbc2144bf50337e57c295121
tulasi-agina <71204135+tulasi-agina@users.noreply.github.com>
2023-05-07 09:40:58 -0700

    Update Shakespeare_Stats.py

--- Shakespeare_Stats.py
+++ Shakespeare_Stats.py
@@ -12,6 +12,8 @@ nltk.download('stopwords')
 
 st.video('https://www.youtu.be/RvHMOwi-ks4')
 
+st.markdown("[![Foo](http://www.google.com.au/images/nav_logo7.png)](http://google.com.au/)")
+
 # Create a dictionary (not a list)
 books = {" ":" ","A Mid Summer Night's Dream":"data/summer.txt","The Merchant of Venice":"data/merchant.txt","Romeo and Juliet":"data/romeo.txt"}
 
