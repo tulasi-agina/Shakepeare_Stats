1aee350330eae60604ad3b6f2ff80aaa619c44cd
tulasi-agina <71204135+tulasi-agina@users.noreply.github.com>
2023-05-07 16:23:02 -0700

    Update ImageGrid.py

--- ImageGrid.py
+++ ImageGrid.py
@@ -6,14 +6,14 @@ st.title('Table of images')
 
 cols = st.columns(3)
 
-cols[0].image('images/row_0_col_0.png', caption='Sentiment Analysis', use_column_width=True)
+cols[0].image('images/row_0_col_0.png', caption='Search Plays', use_column_width=True)
 cols[1].image('images/row_0_col_1.png', caption='Statistical Analysis', use_column_width=True)
-cols[2].image('images/row_0_col_2.png', caption='Text Summary', use_column_width=True)
+cols[2].image('images/row_0_col_2.png', caption='Sentiment Analysis', use_column_width=True)
 
 cols[0].image('images/row_1_col_0.png', caption='AI Play Writer', use_column_width=True)
 cols[1].image('images/row_1_col_1.png', caption='Shakespeare', use_column_width=True)
-cols[2].image('images/row_1_col_2.png', caption='Search Plays', use_column_width=True)
+cols[2].image('images/row_1_col_2.png', caption='Sonnet Clouds', use_column_width=True)
 
-cols[0].image('images/row_2_col_0.png', caption='Infinite Monkey Theorem', use_column_width=True)
-cols[1].image('images/row_2_col_1.png', caption='Random Insults', use_column_width=True)
-cols[2].image('images/row_2_col_2.png', caption='Random Quotes', use_column_width=True)
+cols[0].image('images/row_2_col_0.png', caption='Infinite MonkRandom', use_column_width=True)
+cols[1].image('images/row_2_col_1.png', caption='Random Insults & Quotes', use_column_width=True)
+cols[2].image('images/row_2_col_2.png', caption='Text Summary', use_column_width=True)
